a9d3e50a8f257dce766cf892a5068deb77e2b316
f8coin <shaoying>
2019-05-16 23:55:54 +0800

    修改textarea点击放大

--- js/index.js
+++ js/index.js
@@ -1,3 +1,26 @@
+/* --------------textarea在ios打开点击放大问题----------------- */
+window.onload = function () {
+    document.addEventListener('gesturestart', function (e) {
+        e.preventDefault();
+    });
+    document.addEventListener('dblclick', function (e) {
+        e.preventDefault();
+    });
+    document.addEventListener('touchstart', function (event) {
+        if (event.touches.length > 1) {
+            event.preventDefault();
+        }
+    });
+    var lastTouchEnd = 0;
+    document.addEventListener('touchend', function (event) {
+        var now = (new Date()).getTime();
+        if (now - lastTouchEnd <= 300) {
+            event.preventDefault();
+        }
+        lastTouchEnd = now;
+    }, false);
+};
+
 
 /* --------------添加按钮----------------- */
 $('.addBtn').on('touchend',function(e){
