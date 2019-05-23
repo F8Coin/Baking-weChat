997be074784d3688703ad40c52c81c572279edd7
unknown <59337871@qq.com>
2019-05-23 11:32:44 +0800

    修改js

---
+++ index.js
@@ -0,0 +1,84 @@
+/* --------------textarea在ios打开点击放大问题----------------- */
+window.onload = function () {
+    document.addEventListener('gesturestart', function (e) {
+        e.preventDefault();
+    });
+    document.addEventListener('dblclick', function (e) {
+        e.preventDefault();
+    });
+    document.addEventListener('touchstart', function (event) {
+        if (event.touches.length > 1) {
+            event.preventDefault();
+        }
+    });
+    var lastTouchEnd = 0;
+    document.addEventListener('touchend', function (event) {
+        var now = (new Date()).getTime();
+        if (now - lastTouchEnd <= 300) {
+            event.preventDefault();
+        }
+        lastTouchEnd = now;
+    }, false);
+};
+
+/* --------------文件上传----------------- */
+function uploadFile(inputEle,containerEle) {
+    let formData = new FormData(),
+    fs =inputEle[0].files[0];
+    var reads= new FileReader();
+    reads.readAsDataURL(fs);
+    reads.onload=function (e) {
+        var targetSrc = this.result;
+        containerEle.removeClass('uploadIcon')
+        containerEle.find('.showImg').attr('src',targetSrc);
+    };
+}
+
+
+/* --------------添加按钮----------------- */
+$('.addBtn').on('touchend',function(e){
+    var flag;
+    if($(this).text() != '已添加'){
+        flag = true;
+    }else {
+        flag = false;
+    }
+    if(flag){
+        $(this).html('已添加')
+        $(this).css('backgroundColor','#836FFF');
+        $(this).css('color','#fff');                                    
+        flag = false;
+        e.preventDefault();
+    }else {
+        $(this).html('<span>+</span>添加');
+        $(this).css('backgroundColor','#fff');
+        $(this).css('color','#836FFF');
+        flag = true;
+        e.preventDefault();
+    }
+
+})
+
+/* --------------注销业务----------------- */
+// $('.companyFormContainer .content label>input').on('touchend',function(e){
+//     var selectFlag= true;
+//     if($(this).attr('checked')){
+//         selectFlag = true;
+//     }else {
+//         selectFlag = false;
+//     }
+//     if(selectFlag) {
+//         $(this).attr('checked',false);
+//         selectFlag = false;
+//     }else {
+//         $(this).attr('checked',true);
+//         selectFlag = true;
+//     }
+// })
+
+
+/* --------------办理区域----------------- */
+$('.selectZoom').on('touchend','li',function(e){
+    $(this).addClass('checkedItem').siblings('li').removeClass('checkedItem');
+})    
+ 
