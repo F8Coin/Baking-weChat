f4ad66d46da61d295d30089e6c92e15f8f21fd21
f8coin <shaoying>
2019-05-30 00:25:24 +0800

    修改页面

--- js/index.js
+++ js/index.js
@@ -1,24 +1,24 @@
 /* --------------textarea在ios打开点击放大问题----------------- */
 window.onload = function () {
-    document.addEventListener('gesturestart', function (e) {
-        e.preventDefault();
-    });
-    document.addEventListener('dblclick', function (e) {
-        e.preventDefault();
-    });
-    document.addEventListener('touchstart', function (event) {
-        if (event.touches.length > 1) {
-            event.preventDefault();
-        }
-    });
-    var lastTouchEnd = 0;
-    document.addEventListener('touchend', function (event) {
-        var now = (new Date()).getTime();
-        if (now - lastTouchEnd <= 300) {
-            event.preventDefault();
-        }
-        lastTouchEnd = now;
-    }, false);
+    // document.addEventListener('gesturestart', function (e) {
+    //     e.preventDefault();
+    // });
+    // document.addEventListener('dblclick', function (e) {
+    //     e.preventDefault();
+    // });
+    // document.addEventListener('touchstart', function (event) {
+    //     if (event.touches.length > 1) {
+    //         event.preventDefault();
+    //     }
+    // });
+    // var lastTouchEnd = 0;
+    // document.addEventListener('touchend', function (event) {
+    //     var now = (new Date()).getTime();
+    //     if (now - lastTouchEnd <= 300) {
+    //         event.preventDefault();
+    //     }
+    //     lastTouchEnd = now;
+    // }, false);
 };
 
 /* --------------文件上传----------------- */
