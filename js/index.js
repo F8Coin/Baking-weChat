8943938f1241ae92ffdb3270d803f563d9c099f7
unknown <59337871@qq.com>
2019-10-30 16:59:54 +0800

    增加订单模块功能

--- js/index.js
+++ js/index.js
@@ -153,7 +153,7 @@ function createOrder(url,type,orderInfo,targetUrl) {
             if(res.code == 0) {
                 var orderId= res.data.orderId;
                 sessionStorage.setItem('orderId',orderId)
-                window.location.href = targetUrl;
+                window.location.href = targetUrl+"?orderId="+orderId;
             }else if(res.code == 666){
                 layer.msg(res.msg,function(){
                     window.location.href= 'http://judiaowang.cn/app/view/partnerLogin.html';
@@ -297,3 +297,10 @@ $('.zoomList').on('touchend','li',function(e){
     $(this).addClass('checkedItem').siblings('li').removeClass('checkedItem');
 })
 
+/* --------------获取url中传递的参数----------------- */
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg); //匹配目标参数
+    if (r != null) return unescape(r[2]);
+    return null; //返回参数值
+}
